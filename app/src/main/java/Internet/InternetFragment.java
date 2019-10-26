7e6d5ef23f029bb241398bf79d7367fdc18a5d49
MatheusCBonafe <matheuscbonafe@gmail.com>
2019-10-25 21:33:53 -0300

    Aula 10 - Firebase

--- app/src/main/java/Internet/InternetFragment.java
+++ app/src/main/java/Internet/InternetFragment.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import p185296_m203380.ft.unicamp.aula03_fragmentos.R;
@@ -31,12 +33,26 @@ public class InternetFragment extends Fragment {
 
         final TextView textView = lview.findViewById(R.id.textView);
         final EditText editText = lview.findViewById(R.id.editText);
+        final Spinner spinner = lview.findViewById(R.id.restSpinner);
 
         lview.findViewById(R.id.btnViaCep).setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        new MyViaCepAsyncTask(textView).execute(editText.getText().toString());
+                        String url = "https://aula-10-1d28e.firebaseio.com/" +
+                                editText.getText().toString() + "/.json";
+
+                        String comando = spinner.getSelectedItem().toString();
+
+                        Log.i("InternetFragment", url);
+                        Log.i("InternetFragment", comando);
+
+                        String json = "{\"-Ls49Q18fzRLjBl1WjfC\":{\"bairro\":\"Sé\",\"cep\":\"01001-000\"," +
+                                "\"complemento\":\"lado ímpar\",\"gia\":\"1004\",\"ibge\":\"3550308\"," +
+                                "\"localidade\":\"São Paulo\",\"logradouro\":\"Praça da Sé\"," +
+                                "\"uf\":\"SP\",\"unidade\":\"\"}}";
+
+                        new MyFirstWebClient(textView).execute(url, comando, json);
                     }
                 }
         );
---
+++ app/src/main/java/Internet/MyFirstWebClient.java
@@ -0,0 +1,78 @@
+package Internet;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Objects;
+
+public class MyFirstWebClient extends AsyncTask<String, Void, String> {
+
+    private TextView textView;
+    MyFirstWebClient(TextView textView) {
+        this.textView = textView;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        textView.append("####################### \n ");
+        textView.append("Iniciando ViaCep \n ");
+    }
+
+    @Override
+    protected String doInBackground(String... args) {
+        if (args.length == 3) {
+            return "No Parameter";
+        }
+
+        HttpURLConnection httpURLConnection;
+        try {
+            URL url = new URL(args[0]);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setReadTimeout(10000);
+            httpURLConnection.setConnectTimeout(15000);
+            httpURLConnection.setRequestMethod(args[1]);
+
+            if (args.length == 3) {
+                httpURLConnection.addRequestProperty("Content-Type", "application/json");
+
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+                        httpURLConnection.getOutputStream(), "UTF-8");
+                outputStreamWriter.write(args[2]);
+                outputStreamWriter.flush();
+                outputStreamWriter.close();
+            }
+        /*
+          Lendo a resposta do servidor
+        */
+            BufferedReader reader = new BufferedReader(new
+                    InputStreamReader(httpURLConnection.getInputStream()));
+
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            Log.v("Erro", Objects.requireNonNull(e.getMessage()));
+            return "Exception\n" + e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String args) {
+        textView.setText(args);
+    }
+}
